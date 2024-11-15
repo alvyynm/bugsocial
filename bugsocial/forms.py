3f39faba9224aa036c0d9a91e954e9e8136e198a
alvyynm <alvyynm@gmail.com>
2024-11-15 11:22:14 +0300

    create registration form

--- bugsocial/forms.py
+++ bugsocial/forms.py
@@ -1,6 +1,32 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=80)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    password_confirmation = forms.CharField(
+        widget=forms.PasswordInput, label='Repeat Password')
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password_confirmation(self):
+        """Compare passwords and raise validation error if they don't match
+
+        Raises:
+            forms.ValidationError: Passwords do not match
+
+        Returns:
+            string: password_confirmation field data
+        """
+        data = self.cleaned_data
+
+        if data['password'] != data['password_confirmation']:
+            raise forms.ValidationError("Passwords do not match")
+        return data['password_confirmation']
