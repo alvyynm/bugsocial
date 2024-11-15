73397669812fb87f385d0cae3cec0dd0c428d1cc
alvyynm <alvyynm@gmail.com>
2024-11-15 09:21:47 +0300

    create dashboard url

--- bugsocial/urls.py
+++ bugsocial/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-# from . import views
+from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView(), name='login'),
     path('logout/', auth_views.LogoutView(), name='logout'),
+    path('dashboard/', views.dashboards, name='dashboard')
 ]
