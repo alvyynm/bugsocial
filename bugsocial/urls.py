0c6431cdb15ed830a48a25277f0eedd80fb119b6
alvyynm <alvyynm@gmail.com>
2024-11-15 09:39:13 +0300

    fix errors

--- bugsocial/urls.py
+++ bugsocial/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView(), name='login'),
-    path('logout/', auth_views.LogoutView(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboards, name='dashboard')
 ]
