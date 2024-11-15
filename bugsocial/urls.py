1f585337b96782f838497d1f1325d5e3a32dfe73
alvyynm <alvyynm@gmail.com>
2024-11-15 09:18:53 +0300

    use django auth views for login and logout

--- bugsocial/urls.py
+++ bugsocial/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
+# from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView(), name='login'),
+    path('logout/', auth_views.LogoutView(), name='logout'),
 ]
