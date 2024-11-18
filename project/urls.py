781e0dc33a96c3467c6d7f97ace18f4c889463a2
alvyynm <alvyynm@gmail.com>
2024-11-18 13:40:57 +0300

    install and set up django debug

--- project/urls.py
+++ project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('social/', include('bugsocial.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('bookmarker.urls', namespace='bookmarker')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
