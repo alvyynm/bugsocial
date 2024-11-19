6a52f5125046c6c47f917ce3ddef99372949223b
alvyynm <alvyynm@gmail.com>
2024-11-19 06:56:29 +0300

    add redis to count image views

--- bookmarker/views.py
+++ bookmarker/views.py
@@ -4,10 +4,20 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.conf import settings
+
+import redis
 
 from .forms import ImageCreateForm
 from .models import Image
 from actions.utils import create_action
+
+# connect to Redis
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -38,7 +48,9 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'bookmarker/image/detail.html', {'image': image})
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'bookmarker/image/detail.html',
+                  {'image': image, 'total_views': total_views})
 
 
 @login_required
